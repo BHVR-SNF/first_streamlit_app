092213d6a6531e6a583f0d2df7c510e11fa82a27
BHVR-SNF <145069370+BHVR-SNF@users.noreply.github.com>
2023-09-14 16:00:46 -0600

    Update streamlit_app.py
    added menu for BF

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Idli')
+streamlit.text('Dosa')
+streamlit.text('Vada')
+streamlit.text('Poori')
