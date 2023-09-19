cb1b4b321e3e043e7a2b7a66758e1e7e8844bf61
BHVR-SNF <145069370+BHVR-SNF@users.noreply.github.com>
2023-09-19 16:35:09 -0600

    Update streamlit_app.py
    2 fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,16 +28,15 @@ streamlit.dataframe(fruits_to_show)
 # new section to display fruitivy api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-   Streamlit.error("Please select a fruit to get information")
-else:
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
-
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please Select a fruit to get information")
+  else:
+    back_from_function=get_fruity_vice_data(fruit_choice)
+    # write your own comment - what does this do?
+    streamlit.dataframe(back_from_function)
 except URLError as e:
-streamlit.error()
+  streamlit.error()
 
 # Script break
 streamlit.stop()
